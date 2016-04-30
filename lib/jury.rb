1afe532e8c548927236880061e94e6dc797795c7
LittleStupid <lttlstpd@live.com>
2016-04-30 08:06:54 +0800

    jury test done

--- lib/jury.rb
+++ lib/jury.rb
@@ -10,22 +10,24 @@ class Jury
     @members << member
   end
 
-  def report_votes( votes )
-    puts votes
-
-    votes.each do |vote|
-      puts vote
+  def report_votes( final_votes )
+    final_votes.each do |k,v|
+      puts k.to_s + " " + v.to_s
     end
   end
 
-  def cast_votes( lists )
+  def announce_winner( final_votes )
+    final_votes.keys[0]
+  end
+
+  def cast_votes( final_votes )
     votes = Hash.new
 
     votes_num = Array.new( 2 )
     votes_num[0] = 4
     votes_num[1] = 3
     i = 0
-    lists.each do |item|
+    final_votes.each do |item|
         votes[item] = votes_num[i]
         i += 1
     end
